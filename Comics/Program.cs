caf692dcee26c6637288f180fcfd4dfbb934c011
Bottarte <artem2005zhuk@gmail.com>
2026-09-11 15:33:34 +0300

    Add ActionEndpoint

--- Comics/Program.cs
+++ Comics/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Реєстрація сервісів
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
@@ -12,19 +11,16 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ComicsDbContext>(options =>
     options.UseSqlServer(connectionString));
 
-// Налаштування System.Text.Json для Minimal APIs
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
 {
     options.SerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
 
-// Налаштування System.Text.Json для Controllers (якщо використовуєте і їх)
 builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
 
-// Налаштування CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularJS", policy =>
@@ -37,7 +33,6 @@ var app = builder.Build();
 
 app.UseCors("AllowLiveServer");
 
-// 2. Налаштування Middleware Pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -45,14 +40,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// ВАЖЛИВО: UseCors має йти ПЕРЕД MapComicsEndpoints, MapControllers та UseAuthorization
 app.UseCors("AllowAngularJS");
 
 app.UseAuthorization();
 
-// 3. Маппінг ендпоінтів
 ComicsEndpoints.MapComicsEndpoints(app);
-ReportEndpoints.MapComicsEndpoints(app);
+ReportEndpoints.MapReportsEndpoints(app);
+ActionEndpoints.MapActionEndpoints(app);
 app.MapControllers();
 
 app.Run();
